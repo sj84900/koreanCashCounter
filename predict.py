0937787589dfac459cf7b4f540657ac1997fa0fe
sj84900 <sj84900@gmail.com>
2025-06-20 15:24:28 +0900

    training complete

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 import cv2
 from ultralytics import YOLO
 
-tempImage = "test/images/test01.jpg"
+tempImage = "test/images/3_20231106_172802-35-_jpg.rf.ab69f12ca0956df566ccf17c4ee29ae3.jpg"
 model = YOLO("runs/detect/train/weights/last.pt")
 
 im = cv2.imread(tempImage)
