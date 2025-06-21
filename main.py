13d43e21144c67eb2a855f94b70b8f8e7a4e7ce1
sj84900 <sj84900@naver.com>
2025-06-21 11:10:14 +0900

    웹 구성 완료

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
 from fastapi import FastAPI, Request, File, UploadFile, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import shutil
 import os
 import cv2
+from ultralytics import YOLO
 
 
 app = FastAPI()
-templates = Jinja2Templates(directory="")
+app.mount("/runs", StaticFiles(directory="runs"), name="runs")
+
+templates = Jinja2Templates(directory="templates")
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
+mConvert = [10,50,100,500,1000,5000,10000,50000]
+threshold = 0.8
 
 @app.get("/", response_class=HTMLResponse)
 async def form(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request, "flag":0})
 
-@app.get("/upload", response_class=HTMLResponse)
+@app.post("/upload", response_class=HTMLResponse)
 async def upload(request: Request, file: UploadFile = File(...)):
     filename = os.path.join(UPLOAD_DIR, file.filename)
     with open(filename, "wb") as f:
         shutil.copyfileobj(file.file, f)
-    money = []
-    return templates.TemplateResponse("index.html", {"request": request, "money": money})
+
+    im = cv2.imread(UPLOAD_DIR + "/" + file.filename)
+
+    model = YOLO("usingModel/last.pt")
+
+    result_dir = "runs/detect"
+    if os.path.exists(result_dir + "/latest"):
+        shutil.rmtree(result_dir + "/latest")
+    results = model.predict(source=im, save=True, project=result_dir, name="latest")
+
+    money = [0, 0, 0, 0, 0, 0, 0, 0]
+    print("=" * 30)
+    for result in results:
+        boxes = result.boxes
+        class_ids = boxes.cls  # tensor([4., 5., 6., ...])
+        confidences = boxes.conf  # tensor([0.92, 0.87, ...])
+
+        for cls_id, conf in zip(class_ids, confidences):
+            print(f"인덱스 : {int(cls_id.item())}, 신뢰도 : {float(conf.item()):.2f}")
+            if conf.item() > threshold:
+                money[int(cls_id.item())] = money[int(cls_id.item())] + 1
+    print("=" * 30)
+    total = 0
+    print(money)
+    for idx, value in enumerate(money):
+        total += mConvert[idx] * value
+    print(total)
+    money_data = list(zip(mConvert, money))
+    return templates.TemplateResponse("index.html", {"request": request, "total":total, "money_data": money_data, "flag":1})
--- predict.py
+++ predict.py
@@ -1,11 +1,18 @@
 import cv2
 from ultralytics import YOLO
 
-tempImage = "test/images/3_20231106_172802-35-_jpg.rf.ab69f12ca0956df566ccf17c4ee29ae3.jpg"
+imageLoot = "test/images/"
+imageName = "test01.jpg"
 model = YOLO("runs/detect/train/weights/last.pt")
 
-im = cv2.imread(tempImage)
+im = cv2.imread(imageLoot + imageName)
 results = model.predict(source=im, save=True, save_txt=True)
 print("="*30)
-print(results)
+for result in results:
+    boxes = result.boxes
+    class_ids = boxes.cls  # tensor([4., 5., 6., ...])
+    confidences = boxes.conf  # tensor([0.92, 0.87, ...])
+
+    for cls_id, conf in zip(class_ids, confidences):
+        print(f"인덱스 : {int(cls_id.item())}, 신뢰도 : {float(conf.item()):.2f}")
 print("="*30)
